596dda5eec1c5c3c1c4b28d570dbe96b7528ed8d
Agustin Demontel <agustindemontel@gmail.com>
2025-05-07 23:48:18 -0300

    Refactor CSS and Vite configuration for Tailwind CSS integration; remove unused PostCSS config

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -1,12 +1,12 @@
 // vite.config.mjs
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwind from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
 	react(),
-	tailwind(),
+	tailwindcss(),
 	],
   resolve: {
     extensions: ['.js', '.jsx']
